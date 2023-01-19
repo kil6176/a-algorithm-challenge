93789687b81e1c6e4f96872f6ef6fae64d7c4393
dkssudtp1 <dkssudtp1@naver.com>
2023-01-19 19:46:06 +0900

    REFACTOR : 프로그래머스 양식에 맞게 수정

--- src/kimdonghyeon/1day/ArrayAverage.java
+++ src/kimdonghyeon/1day/ArrayAverage.java
@@ -1,14 +1,26 @@
+//정수 배열 numbers가 매개변수로 주어집니다.
+// numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+//입출력 예
+//        numbers	result
+//        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]	5.5
+//        [89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]	94.0
+//        입출력 예 설명
+//        입출력 예 #1
+//        numbers의 원소들의 평균 값은 5.5입니다.
+//        입출력 예 #2
+//        numbers의 원소들의 평균 값은 94.0입니다.
 public class ArrayAverage {
+    public static double solution(int[] numbers) {
+        double answer = 0;
+
+        for (int arr : numbers) answer += arr;
+
+        return answer/numbers.length;
+    }
     public static void main(String[] args) {
-        //int []arrInt = {1, 2, 3, 4, 5};
-        int []arrInt = {89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
-        int totalNum = 0;
+        //int []numbers = {1, 2, 3, 4, 5};
+        int []numbers = {89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
 
-
-        for (int arr : arrInt)
-            totalNum += arr;
-
-
-        System.out.println((float) totalNum/arrInt.length);
+        System.out.println(solution(numbers));
     }
 }
--- src/kimdonghyeon/1day/SumEvenNumber.java
+++ src/kimdonghyeon/1day/SumEvenNumber.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
-
+//정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+// 입출력 예 설명
+//입출력 예 #1
+//n이 10이므로 2 + 4 + 6 + 8 + 10 = 30을 return 합니다.
+//입출력 예 #2
+//n이 4이므로 2 + 4 = 6을 return 합니다.
 public class SumEvenNumber
 {
+
+        public static int solution(int n) {
+            int answer = 0;
+
+            for(int i=0; i<=n; i++) answer += (i % 2 == 0) ? i : 0;
+
+            return answer;
+        }
+
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
         int inputNumber = scan.nextInt();
-        int evenTotal = 0;
-        if(0 < inputNumber && inputNumber <= 1000)
-        {
-            for(int i=0; i<=inputNumber; i++)
-                evenTotal += (i % 2 == 0) ? i : 0;
-        }
-        System.out.print(evenTotal);
+
+        System.out.print(solution(inputNumber));
 
     }
 }
